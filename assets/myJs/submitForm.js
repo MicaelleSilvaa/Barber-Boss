e743de15f388d37274cf16c31706fc5569177788
MicaelleSilvaa <micaelle.silva@liferay.com>
2024-03-17 15:37:08 -0300

    up img and favicon

--- assets/myJs/submitForm.js
+++ assets/myJs/submitForm.js
@@ -1,12 +1,12 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   const form = document.getElementById('appointment-form');
   const submitBtn = document.getElementById('submit-btn');
   const successMessage = document.getElementById('success-message');
 
-  form.addEventListener('submit', function(event) {
-    event.preventDefault(); 
+  form.addEventListener('submit', function (event) {
+    event.preventDefault();
 
-    setTimeout(function() {
+    setTimeout(function () {
       successMessage.classList.add('show');
     }, 1000);
   });
