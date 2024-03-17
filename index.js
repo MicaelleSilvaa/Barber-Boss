36dda46e1e6448ea6355cf3c7415627da4256cd3
MicaelleSilvaa <micaelle.silva@liferay.com>
2024-03-17 17:29:17 -0300

    testing pwa

---
+++ index.js
@@ -0,0 +1,9 @@
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("service-worker.js").then(registration => {
+        console.log("SW registered!");
+        console.log(registration);
+    }).catch(error => {
+        console.log("SW registration failed");
+        console.log(error);
+    })
+}
---
+++ service-worker.js
@@ -0,0 +1,41 @@
+const CACHE_VERSION = 'v1';
+const CACHE_FILES = [
+    '/',
+    '/index.html',
+    '/assets/css/main.css',
+    '/index.js',
+    '/assets/img/144.png'
+];
+
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+        caches.open(CACHE_VERSION).then(function (cache) {
+            return cache.addAll(CACHE_FILES);
+        })
+    );
+});
+
+self.addEventListener('activate', function (event) {
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.map(function (cacheName) {
+                    if (cacheName !== CACHE_VERSION) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            if (response) {
+                return response;
+            }
+            return fetch(event.request);
+        })
+    );
+});
