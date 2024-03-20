702ab9eb0dad07ee46ac064f9cba992aff0d9638
MicaelleSilvaa <micaelle.silva@liferay.com>
2024-03-20 19:29:59 -0300

    up

--- service-worker.js
+++ service-worker.js
@@ -3,22 +3,13 @@ const CACHE_FILES = [
     '/index.html',
     '/contact.html',
     '/assets/css/main.css',
-    '/assets/js/index.js',
+    '/index.js',
     '/assets/img/144.png',
     '/assets/img/why-us-bg.jpg',
     '/assets/img/hero-bg.jpg',
     '/assets/vendor/swiper/swiper-bundle.min.js',
-    '/assets/vendor/swiper/swiper-bundle.min.css',
-    '/assets/vendor/bootstrap/css/bootstrap.min.css',
-    '/assets/vendor/bootstrap/js/bootstrap.bundle.min.js',
-    '/assets/vendor/aos/aos.css',
-    '/assets/vendor/aos/aos.js',
-    '/assets/vendor/glightbox/css/glightbox.min.css',
-    '/assets/vendor/glightbox/js/glightbox.min.js',
-    '/assets/vendor/remixicon/remixicon.css',
-    '/assets/vendor/bootstrap-icons/bootstrap-icons.css',
-    '/assets/vendor/isotope-layout/isotope.pkgd.min.js',
-    '/assets/vendor/php-email-form/validate.js'
+    '/assets/vendor//swiper/swiper-bundle.min.css',
+    './'
 ];
 
 self.addEventListener('install', function (event) {
