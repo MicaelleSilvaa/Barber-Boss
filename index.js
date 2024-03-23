6f529855fc0b09a9d018ca7e166e8b4b17238583
MicaelleSilvaa <micaelle.silva@liferay.com>
2024-03-22 21:27:14 -0300

    test

--- index.js
+++
@@ -1,9 +0,0 @@
-if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("service-worker.js").then(registration => {
-        console.log("SW registered!");
-        console.log(registration);
-    }).catch(error => {
-        console.log("SW registration failed");
-        console.log(error);
-    })
-}
