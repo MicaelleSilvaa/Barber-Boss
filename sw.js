ad6a83be3f0831008ffd716db9ecb2fc590a049e
micaellesilvaa <micaelle.silva@liferay.com>
2024-03-26 09:09:20 -0300

    FIX: test sw

--- sw.js
+++ sw.js
@@ -1,47 +1,52 @@
-// This is the "Offline page" service worker
+var cacheName = 'BarberBoss';
 
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+self.addEventListener('install', event => {
 
-const CACHE = "BarberBoss";
+  self.skipWaiting();
 
-// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-const offlineFallbackPage = "index.html";
+  event.waitUntil(
+    caches.open(cacheName)
+      .then(cache => cache.addAll([
 
-self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
+        'index.html',
+        'contact.html',
+        'about.html',
+        'services.html',
+        'takePhoto.html',
+
+        './assets/js/main.js',
+
+        './assets/img/cards-3.jpg',
+        './assets/img/why-us-bg.png',
+
+      ]))
+  );
+});
+
+self.addEventListener('message', function (event) {
+  if (event.data.action === 'skipWaiting') {
     self.skipWaiting();
   }
 });
 
-self.addEventListener('install', async (event) => {
-  event.waitUntil(
-    caches.open(CACHE)
-      .then((cache) => cache.add(offlineFallbackPage))
-  );
-});
+self.addEventListener('fetch', function (event) {
+  //Atualizacao internet
+  event.respondWith(async function () {
+    try {
+      return await fetch(event.request);
+    } catch (err) {
+      return caches.match(event.request);
+    }
+  }());
 
-if (workbox.navigationPreload.isSupported()) {
-  workbox.navigationPreload.enable();
-}
-
-self.addEventListener('fetch', (event) => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith((async () => {
-      try {
-        const preloadResp = await event.preloadResponse;
-
-        if (preloadResp) {
-          return preloadResp;
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        if (response) {
+          return response;
         }
+        return fetch(event.request);
+      })
+  );
 
-        const networkResp = await fetch(event.request);
-        return networkResp;
-      } catch (error) {
-
-        const cache = await caches.open(CACHE);
-        const cachedResp = await cache.match(offlineFallbackPage);
-        return cachedResp;
-      }
-    })());
-  }
 });
