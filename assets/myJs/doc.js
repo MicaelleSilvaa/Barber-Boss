c075a2da5b7b4ecce89204e97733cc5ea3c16010
MicaelleSilvaa <micaelle.silva@liferay.com>
2024-04-02 20:14:08 -0300

    Fix: Test Wp

--- assets/myJs/doc.js
+++ assets/myJs/doc.js
@@ -15,14 +15,21 @@ const captureBtn = document.getElementById('capture-btn');
 
 captureBtn.addEventListener('click', function () {
     const canvas = document.createElement('canvas');
-    canvas.width = video.videoWidth;
-    canvas.height = video.videoHeight;
+    const scale = 0.5;
+    canvas.width = video.videoWidth * scale;
+    canvas.height = video.videoHeight * scale;
     const ctx = canvas.getContext('2d');
     ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
 
-    // Convertendo a imagem para base64
-    const capturedImageDataUrl = canvas.toDataURL('image/jpeg', 0.5); // Altere a qualidade conforme necessário
+    // Convertendo a imagem para base64 com compressão
+    canvas.toBlob(blob => {
+        const reader = new FileReader();
+        reader.readAsDataURL(blob);
+        reader.onloadend = function () {
+            const capturedImageDataUrl = reader.result;
 
-    // Redirecionar para a página de contato com a imagem em base64 na URL
-    window.location.href = 'contact.html?image=' + encodeURIComponent(capturedImageDataUrl);
+            // Redirecionar para a página de contato com a imagem em base64 na URL
+            window.location.href = 'contact.html?image=' + encodeURIComponent(capturedImageDataUrl);
+        };
+    }, 'image/jpeg', 0.5); // Altere a qualidade conforme necessário
 });
